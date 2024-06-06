877d581488cdb4c5ba75b0565f4282cb52d71057
tnkcoll <theodore.collins2023@outlook.com>
2024-06-06 15:25:01 -0500

    ChallengeLabsWeek5.2

--- Program.cs
+++ Program.cs
@@ -7,8 +7,13 @@ namespace ChallengeLabsWeek5._2
     {
         static void Main(string[] args)
         {
+            //Call the method to get the numbers from the user.
             int[] ints = GetArrayOfIntsFromUser("Please enter a range of numbers in sequence that is missing one digit. . Ex: 5412, sequence from 1-5 missing the 3.");
+            
+            //Send the array of ints to the method to get the missing number.
             int missingNumber = GetMissingInt(ints);
+            
+            //If the missing number we received form the GetMissingInt method is not -1, print the missing number.
             if (missingNumber != -1)
             {
                 Console.WriteLine("The missing number is: {0}", missingNumber);
@@ -29,8 +34,10 @@ namespace ChallengeLabsWeek5._2
                 userInput = Console.ReadLine();
             } while (userInput == "");
 
+            //Instatntiate a new array of ints with a length of the user's number.
             int[] ints = new int[userInput.Length];
 
+            //Get the numeric value of each number in the user's multi-digit number.
             for (int i = 0; i < userInput.Length; i++)
             {
                 double num = char.GetNumericValue(userInput[i]);
@@ -41,23 +48,28 @@ namespace ChallengeLabsWeek5._2
 
         static int GetMissingInt(int[] array)
         {
+            int minNumber = array.Min();
+            int maxNumber = array.Max();
   
-
+            //Instantiate a new dictionary to hold the ints from the int array we created in the GetArrayOfIntsFromUser method.
             Dictionary<int,int> numbers = new Dictionary<int,int>();
 
+            //Populate the dictionary with the ints from the array.
             for (int i = 0; i < array.Length; i++)
             {
-                if (numbers.ContainsKey(array[i]))
-                {
-                    numbers[array[i]]++;
-                }
-                else
+                if (!numbers.ContainsKey(array[i]))
                 {
                     numbers[array[i]] = 1;
                 }
+                else
+                {
+                    numbers[array[i]] = 0;
+                }
             }
 
-            for (int i = 1; i < array.Length; i++)
+
+            //Parse the dictionary to see i
+            for (int i = minNumber; i < maxNumber; i++)
             {
                 if (!numbers.ContainsKey(i))
                 {
